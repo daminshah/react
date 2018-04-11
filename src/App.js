6d61553ed3ce4e1c9eda2678e190b59c60f24da2
daminshah <daminshah210@gmail.com>
2018-04-10 17:21:42 -0700

    Implemented Lists and conditional statements in react

--- src/App.js
+++ src/App.js
@@ -7,32 +7,52 @@ import Person from './Person/Person'
 class App extends Component {
   state={
     persons:[
-      {name:'A',age:21},
-      {name:'B',age:22},
-      {name:'C',age:23},
+      {id:'asd1',name:'A',age:21},
+      {id:'dsd1',name:'B',age:22},
+      {id:'dewd1',name:'C',age:23},
 
     ]
   }
-  switchNameHandler=(newName)=>{
-    //console.log('Was clicked')
-    //this.state.persons[0].name='AAAAA'
-    this.setState({
-      persons:[
-      {name:newName,age:21},
-      {name:'B',age:22},
-      {name:'C',age:26},
-      ]
-    })
-  }
-  nameChangedHandler=(event)=>{
-    this.setState({
-      persons:[
-      {name:'A',age:21},
-      {name:event.target.value,age:22},
-      {name:'C',age:26},
-      ]
-    })
+  // switchNameHandler=(newName)=>{
+  //   //console.log('Was clicked')
+  //   //this.state.persons[0].name='AAAAA'
+  //   this.setState({
+  //     persons:[
+  //     {name:newName,age:21},
+  //     {name:'B',age:22},
+  //     {name:'C',age:26},
+  //     ]
+  //   })
+  // }
+  nameChangedHandler=(event,id)=>{
 
+    const personIndex=this.state.persons.findIndex(p=>{
+      return p.id===id;
+    });
+    //console.log(personIndex)
+
+    const person={
+      ...this.state.persons[personIndex]
+    };
+    //console.log(person)
+    person.name=event.target.value;
+    const persons=[...this.state.persons]
+    
+    persons[personIndex]=person;
+    this.setState({ persons:persons })
+    console.log(persons)
+
+
+}
+deletePersonHandler=(personIndex)=>{
+  //const persons=this.state.persons.slice();
+  const persons=[...this.state.persons]
+  persons.splice(personIndex,1);
+  this.setState({persons:persons})
+}
+  
+  togglePersonsHandler=()=>{
+    this.setState({showPersons:!this.state.showPersons})
   }
   render() {
     const style={
@@ -43,20 +63,32 @@ class App extends Component {
       cursor:'pointer'
     }
 
+    let persons=null;
+    if(this.state.showPersons){
+
+      persons=(
+        <div>
+          {this.state.persons.map((person,index)=>{
+            return <Person 
+            click={()=>this.deletePersonHandler(index)}
+            name={person.name} 
+            age={person.age}
+            key={person.id}
+            changed={(event)=>this.nameChangedHandler(event,person.id)}/>
+          })}
+       
+   
+    </div>
+      );
+    }
+
     return ( 
     <div className = "App" >
     <h1 > Hi, I 'm a react app</h1> 
     <button 
     style={style}
-    onClick={() =>this.switchNameHandler('AAA!!!')}>Switch Names</button> 
-    <Person name={this.state.persons[0].name} 
-    age={this.state.persons[0].age} />
-    <Person name={this.state.persons[1].name} 
-    age={this.state.persons[1].age}
-     click={this.switchNameHandler.bind(this,'A!')}
-     changed={this.nameChangedHandler}>My Hobbies:Racing</Person>
-    <Person name={this.state.persons[2].name} 
-   age={this.state.persons[2].age}/>
+    onClick={this.togglePersonsHandler}>Switch Names</button>
+    {persons}
     </div >
     // <h1> Another Heading</h1>
     
